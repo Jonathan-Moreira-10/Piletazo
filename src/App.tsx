8ce024a0f85592f2b721cf90f497084de004689c
Jonathan Moreira <jonathan.ariel.moreira@gmail.com>
2026-08-12 19:04:33 -0300

    Primer Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <h1>Vite + React</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
